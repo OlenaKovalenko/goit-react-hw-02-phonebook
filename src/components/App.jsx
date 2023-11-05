c06f5a7f4322d6bff723ca96580f1118c3745045
OlenaKovalenko <elenkow17@gmail.com>
2023-11-05 22:36:22 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { GlobalStyle } from './GlobalStyle';
 
 
 export class App extends Component {
   state = { 
-    contacts: [],
-    filter: ''
+    // contacts: [],
+    // filter: ''
   } 
   render() { 
     return (
