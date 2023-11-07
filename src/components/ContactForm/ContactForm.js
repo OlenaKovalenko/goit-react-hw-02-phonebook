7deb74922b8fdcfc159431bb0d1f0ea98a3ba4ff
OlenaKovalenko <elenkow17@gmail.com>
2023-11-07 21:46:33 +0200

    Update ContactForm.js

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,11 +1,20 @@
-import { Formik, Field } from 'formik';
-// import * as Yup from 'yup';
+import { Formik, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import { StyledForm } from './ContactForm.styled';
 
-// const schema = Yup.object().shape({
-//     name: Yup.string().required('Required'),
-//     number: Yup.number().required('Required'),
-// });
+const validationSchema = Yup.object().shape({
+    name: Yup.string()
+    .min(2, 'Name must contain at least 2 characters')
+    .max(40, 'Too long name')
+    .required('Name is required!'),
+    number: Yup.string()
+    .matches(/^\+(?:[0-9] ?){6,14}[0-9]$/, {
+        message: 'Invalid Phone Number!',
+        excludeEmptyString: false,
+    })
+    .required('Phone number is required!')
+    .max(15, 'Invalid phone number!'),
+});
 
 export const ContactForm =({ onAdd }) => (
         <Formik
@@ -14,7 +23,7 @@ export const ContactForm =({ onAdd }) => (
             number: '',
             }}
             
-            // validationSchema={schema}
+            validationSchema={validationSchema}
 
             onSubmit={(values, actions) => {
                 onAdd(values);
@@ -23,10 +32,13 @@ export const ContactForm =({ onAdd }) => (
         >
             <StyledForm>
                 <label htmlFor="name">Name</label>
-                <Field type="text" name="name" required />
+                <Field type="text" name="name"  placeholder="John Smith" />
+                <ErrorMessage name="name" component="div" />
 
                 <label htmlFor="number">Number</label>
-                <Field type="tel" name="number" required />
+                <Field type="tel" name="number" placeholder="+XX..." />
+                <ErrorMessage name="number" component="div" />
+            
                 
                 <button type="submit">Add contact</button>
             </StyledForm>
